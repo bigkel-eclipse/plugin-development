6d663e86b0b5420db0f60afa64dfdc07b58ce0aa
kw520 <1151094976@qq.com>
2022-12-27 17:51:48 +0800

    EMF change

--- EMF/Library/com.itchenyang.library.editor/src/com/itchenyang/library/test/testSave.java
+++ EMF/Library/com.itchenyang.library.editor/src/com/itchenyang/library/test/testSave.java
@@ -1,41 +1,33 @@
 package com.itchenyang.library.test;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
-
-import com.itchenyang.library.Book;
-import com.itchenyang.library.Library;
-import com.itchenyang.library.LibraryFactory;
-import com.itchenyang.library.Writer;
-
 public class testSave {
 	public static void main(String[] args) {
-
-		Library library = LibraryFactory.eINSTANCE.createLibrary();
 		
-		Writer writer = LibraryFactory.eINSTANCE.createWriter();
-		writer.setName("bigkel.chen");
-
-		Book book = LibraryFactory.eINSTANCE.createBook();
-		book.setTitle("cjkcare.top");
-		book.setAuthor(writer);
-
-		library.getWriters().add(writer);
-		library.getBooks().add(book);
+		String aaa = "/ggg/   gg/bbbbbb%20aaaaa";
+		System.out.println(aaa.replaceAll("%20", " "));
 		
-		URI fileURI = URI.createFileURI(new File("E:\\×ÀÃæ\\mylibrary.xml").getAbsolutePath());
-		Resource resource = new XMLResourceImpl(fileURI);
-		resource.getContents().add(library);
-		try {
-			resource.save(null);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+//		Library library = LibraryFactory.eINSTANCE.createLibrary();
+//		
+//		Writer writer = LibraryFactory.eINSTANCE.createWriter();
+//		writer.setName("bigkel.chen");
+//
+//		Book book = LibraryFactory.eINSTANCE.createBook();
+//		book.setTitle("cjkcare.top");
+//		book.setAuthor(writer);
+//
+//		library.getWriters().add(writer);
+//		library.getBooks().add(book);
+//		
+//		URI fileURI = URI.createFileURI(new File("E:\\×ÀÃæ\\mylibrary.xml").getAbsolutePath());
+//		Resource resource = new XMLResourceImpl(fileURI);
+//		resource.getContents().add(library);
+//		try {
+//			resource.save(null);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
 //		ResourceSet resourceSet = new ResourceSetImpl();
 //		
